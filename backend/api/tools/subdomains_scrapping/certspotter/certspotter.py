f24a6dde4b9be718751b83b344cd5c3ba38d3e38
Abdulrahman <227abdulrahman@gmail.com>
2025-11-06 12:55:27 +0200

    Adding CertSpotter as a provider.

---
+++ backend/api/tools/subdomains_scrapping/certspotter/certspotter.py
@@ -0,0 +1,29 @@
+import yaml, requests, json
+
+with open('/work/backend/.config/provider.yaml') as file:
+    data = yaml.safe_load(file)
+data = data["certspotter"]
+key = data["apikey"]
+
+
+def scrap(domain):
+
+    url = f"https://api.certspotter.com/v1/issuances?domain={domain}&include_subdomains=true&expand=dns_names"
+    headers = {
+        "Authorization" : f"Bearer {key}",
+    }
+    try:
+        response = requests.get(url, headers=headers)
+        response = response.json()
+
+        subdomains = set()
+
+        for cert in response:
+            for sub in cert["dns_names"]:
+                subdomains.add(sub)
+
+        return subdomains
+
+    except Exception:
+        return set()
+
