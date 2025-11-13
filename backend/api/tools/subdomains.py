1c4268ee67313baedcb6e4919c94bb6b0e85b647
227abdulrahman <227abdulrahman@gmail.com>
2025-11-13 08:11:27 +0200

    Adding SecurityTrails through web scrapping

--- backend/api/tools/subdomains.py
+++ backend/api/tools/subdomains.py
@@ -1,14 +1,24 @@
 import sys
 import os
+from pathlib import Path
+from subdomains_scrapping.securityTrails.securityTrails import scrap as securityTrailsScrap
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, current_dir)
 
+def enum(domain):
+    output_dir = f"/work/backend/api/tools/output/{domain}"
+    p = Path(output_dir)
+    p.mkdir(parents=True, exist_ok=True)
 
+    securityTrailsResult = securityTrailsScrap(domain)
 
-from subdomains_scrapping.alienVault.alienVault import scrap as scrap
+    if securityTrailsResult == {-1}:
+        print("Security Trails key expired.")
+    else:
+        with open(f"{output_dir}/securityTrails.txt", "w") as file:
+            for subdomain in securityTrailsResult:
+                file.write(f"{subdomain}\n")
+    pass
 
-
-for i in scrap("jobs.ch"):
-    print(i)
-
+enum("jobs.ch")
