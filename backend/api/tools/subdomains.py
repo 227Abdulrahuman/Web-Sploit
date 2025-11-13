631d5f32ca73057004656bad908ff651aad8ce3d
227abdulrahman <227abdulrahman@gmail.com>
2025-11-13 09:20:35 +0200

    Adding Digital Yama as a provider.

--- backend/api/tools/subdomains.py
+++ backend/api/tools/subdomains.py
@@ -2,6 +2,7 @@ import sys
 import os
 from pathlib import Path
 from subdomains_scrapping.securityTrails.securityTrails import scrap as securityTrailsScrap
+from subdomains_scrapping.digitalYama.digitalYama import scrap as digitalYamaScrap
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, current_dir)
@@ -11,14 +12,23 @@ def enum(domain):
     p = Path(output_dir)
     p.mkdir(parents=True, exist_ok=True)
 
+    #1) Security Trails
     securityTrailsResult = securityTrailsScrap(domain)
-
     if securityTrailsResult == {-1}:
         print("Security Trails key expired.")
     else:
         with open(f"{output_dir}/securityTrails.txt", "w") as file:
             for subdomain in securityTrailsResult:
                 file.write(f"{subdomain}\n")
-    pass
+
+
+    #2) Digital Yama
+    digitalYamaResult = digitalYamaScrap(domain)
+    if digitalYamaResult == {-1}:
+        print("Digital Yama key expired.")
+    else:
+        with open(f"{output_dir}/digitalYama.txt", "w") as file:
+            for subdomain in digitalYamaResult:
+                file.write(f"{subdomain}\n")
 
 enum("jobs.ch")
---
+++ backend/api/tools/subdomains_scrapping/digitalYama/digitalYama.py
@@ -0,0 +1,24 @@
+import requests, yaml, json
+
+with open('/work/backend/.config/provider.yaml') as file:
+    data = yaml.safe_load(file)
+
+key = data["DigitalYama"]["apiKey"]
+def scrap(domain):
+    url = f"https://api.digitalyama.com/subdomain_finder?domain={domain}"
+    headers = {
+        "x-api-key" : key
+    }
+    subdomains = set()
+
+    try:
+        response = requests.get(url,headers=headers)
+        if 400 <= response.status_code < 500:
+            return {-1}
+
+        res = response.json()
+        for sub in res["subdomains"]:
+            subdomains.add(sub)
+        return subdomains
+    except Exception:
+        return set()
