ecfd995db18d4c96c97fcec4ce95903fc66b592f
227abdulrahman <227abdulrahman@gmail.com>
2025-11-20 07:01:27 +0200

    Adding Leakix

---
+++ backend/api/tools/subdomains_scrapping/leakix/leakix.py
@@ -0,0 +1,26 @@
+import yaml, requests
+
+with open('/work/backend/.config/provider.yaml') as file:
+    data = yaml.safe_load(file)
+key = data["Leakix"]["apiKey"]
+
+def scrap(domain):
+    url = f"https://leakix.net/api/subdomains/{domain}"
+    headers = {
+        "Accept": "application/json",
+        "api-key": key
+    }
+    response = requests.get(url, headers=headers)
+    if 400 <= response.status_code < 500:
+        return {-1}
+
+    response = response.json()
+
+    subdomains = set()
+    for item in response:
+        subdomains.add(item["subdomain"])
+
+    return subdomains
+
+for sub in scrap("jobs.ch"):
+    print(sub)
