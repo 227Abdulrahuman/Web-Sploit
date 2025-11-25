3c98fac90e84e5d9a88e7090d248547780afe0f9
Abdulrahman <147337877+227Abdulrahuman@users.noreply.github.com>
2025-11-25 18:28:21 +0200

    Adding Celery (#27)

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -3,6 +3,11 @@ from django.urls import path
 from ninja import NinjaAPI
 from core.models import Domain
 import json
+import time
+from celery import shared_task
+from api.tasks import test2
+
+
 api = NinjaAPI()
 
 
@@ -10,7 +15,11 @@ api = NinjaAPI()
 def add(request, a: int, b: int):
     return {"result": a + b}
 
-
+#Test Celery Endpoint.
+@api.get("/celery")
+def run(request, a:int):
+    test2.delay()
+    return {"status": 200}
 
 
 urlpatterns = [
